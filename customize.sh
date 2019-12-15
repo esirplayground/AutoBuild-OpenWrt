73e16e2212f5ec73b2b2ac8bc74ae4e26294538c
eSir <50467624+esirplayground@users.noreply.github.com>
2019-12-14 22:29:49 -0500

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -5,5 +5,5 @@
 # Author: eSirPlayground
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
-Modify default IP
+#1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate

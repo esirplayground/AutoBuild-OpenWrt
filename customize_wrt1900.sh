bd3ec419e3b8adedc62bdded6103ffff9d22a369
yasum2006 <32138091+yasum2006@users.noreply.github.com>
2019-12-18 16:41:56 +0800

    Create customize_wrt1900.sh

---
+++ customize_wrt1900.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#=================================================
+# Description: DIY script
+# Lisence: MIT
+# Author: eSirPlayground
+# Youtube Channel: https://goo.gl/fvkdwm 
+#=================================================
+#1. Modify default IP
+sed -i 's/192.168.1.1/192.168.8.192/g' openwrt/package/base-files/files/bin/config_generate

90d1ffcccbed925ece52e6020430a27587b81e06
nchen194 <47018658+nchen194@users.noreply.github.com>
2019-12-19 23:49:07 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,11 +8,10 @@
 #1. Modify default IP
 sed -i 's/192.168.1.1/10.10.10.6/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/192.168.$((addr_offset++)).1/10.10.$((addr_offset++)).4/g' openwrt/package/base-files/files/bin/config_generate
-./scripts/feeds update luci-lib-json
-./scripts/feeds install luci-lib-json
-cd ./package
+openwrt/scripts/feeds update luci-lib-json
+openwrt/scripts/feeds install luci-lib-json
+cd openwrt/package
 git clone https://github.com/tty228/luci-app-serverchan.git
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git
-./scripts/feeds install luci-lib-json
 git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker
 git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman

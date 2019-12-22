34c4979145f06d63618c6b2dadf5060e1778182b
nchen194 <47018658+nchen194@users.noreply.github.com>
2019-12-22 11:04:09 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,6 +6,7 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP 设置编译后本机IP，等同vi /etc/sysconfig/network
+ecgo -e "src-git lienol https://github.com/Lienol/openwrt-package >> openwrt/feeds.conf.default
 sed -i 's/192.168.1.1/10.10.10.6/g' openwrt/package/base-files/files/bin/config_generate
 sed -i 's/192.168.$((addr_offset++)).1/10.10.$((addr_offset++)).4/g' openwrt/package/base-files/files/bin/config_generate
 #下载自定义固件，需要配合config文件使用

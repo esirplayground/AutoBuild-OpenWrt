aa50b67844d53221eeeb9b80d1638db2af5bb4be
nchen194 <47018658+nchen194@users.noreply.github.com>
2020-02-28 18:59:20 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -17,3 +17,4 @@ git clone https://github.com/tty228/luci-app-serverchan.git package/lean/luci-ap
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git package/luci-app-adguardhome
 git clone https://github.com/lisaac/luci-lib-docker.git package/luci-lib-docker 
 # git clone https://github.com/lisaac/luci-app-dockerman.git package/luci-app-dockerman 
+#1

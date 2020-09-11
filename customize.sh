22e2dc4c007f4049f2abf040c5dc43bde3ce2408
stephenchou888 <48269736+stephenchou888@users.noreply.github.com>
2020-09-11 14:58:57 +0800

    Update customize.sh
    Add a feed source
    helloworld feeds
    passwall feeds

--- customize.sh
+++ customize.sh
@@ -7,3 +7,7 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+
+# Add a feed source
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default

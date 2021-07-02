91218bf8a82dae51895c750d2a3eb16ba005c333
eSir <50467624+esirplayground@users.noreply.github.com>
2021-07-02 19:54:45 -0400

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -7,3 +7,5 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+#2. Replace with JerryKuKu’s Argon
+#rm package/lean/luci-theme-argon -rf

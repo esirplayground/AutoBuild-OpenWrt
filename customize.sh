f2d4add776b2d5c8557dd639d63b63614cc61941
exLuLu <35789196+exLuLu@users.noreply.github.com>
2021-07-03 09:34:15 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -9,3 +9,5 @@
 sed -i 's/192.168.1.1/192.168.50.22/g' openwrt/package/base-files/files/bin/config_generate
 #2. Replace with JerryKuKu’s Argon
 rm package/lean/luci-theme-argon -rfv
+#3. Replace with JerryKuKu’s jd-dailybonus
+rm package/lean/luci-app-jd-dailybonus -rfv

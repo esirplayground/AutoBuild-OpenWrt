7b1854a54fcdcbff55f1c1a9805cf087f8f3b097
exLuLu <35789196+exLuLu@users.noreply.github.com>
2021-07-03 22:32:33 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,6 +8,6 @@
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.50.22/g' openwrt/package/base-files/files/bin/config_generate
 #2. Replace with JerryKuKu’s Argon
-#rm package/lean/luci-theme-argon -rf
+rm openwrt/package/lean/luci-theme-argon -rf
 #3. Replace with JerryKuKu’s jd-dailybonus
-#rm package/lean/luci-app-jd-dailybonus -rf
+rm openwrt/package/lean/luci-app-jd-dailybonus -rf

95fe1eab14b8306017ec235ffb16dfa0892eeba1
Raphael <Azreulo@Gmail.Com>
2021-07-07 10:53:15 +0800

    Raphael's Customize

--- customize.sh
+++ customize.sh
@@ -7,4 +7,4 @@
 #=================================================
 #1. Modify default IP
 sed -i 's/192.168.1.1/10.0.0.100/g' openwrt/package/base-files/files/bin/config_generate
-sed -i 's/OpenWrt/ENDLESS/g' package/base-files/files/bin/config_generate
+sed -i 's/OpenWrt/ENDLESS/g' openwrt/package/base-files/files/bin/config_generate

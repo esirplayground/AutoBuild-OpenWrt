f62ace2502e841b85b8b5ef1061654bc548bf080
exLuLu <xskylulux@gmail.com>
2021-11-27 14:30:19 +0800

    使用github actions cache缓存工具链
    https://github.com/coolsnowwolf/lede/pull/7796

--- customize.sh
+++ customize.sh
@@ -3,11 +3,13 @@
 # Description: DIY script
 # Lisence: MIT
 # Author: eSirPlayground
-# Youtube Channel: https://goo.gl/fvkdwm 
+# Youtube Channel: https://goo.gl/fvkdwm
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.50.22/g' openwrt/package/base-files/files/bin/config_generate
-#2. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rfv
-#3. Replace with JerryKuKu’s jd-dailybonus
-#rm openwrt/package/lean/luci-app-jd-dailybonus -rfv
+sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+
+#2. Clear the login password
+sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#3. Replace with JerryKuKu’s Argon
+#rm openwrt/package/lean/luci-theme-argon -rf

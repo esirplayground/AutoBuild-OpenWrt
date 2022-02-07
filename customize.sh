f7163cb9f82e33e8636b5a50bf696b8a78a8d77b
ydhang <42969436+ydhang@users.noreply.github.com>
2022-02-07 11:09:05 +0800

    update

--- customize.sh
+++ customize.sh
@@ -6,10 +6,19 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
+#sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
-#3. Replace with JerryKuKu’s Argon
-#rm openwrt/package/lean/luci-theme-argon -rf
+#3. Modify the Compiler
+sed -i 's/R22.1.1/R22.1.1 Compiled by ydhang/g' openwrt/package/lean/default-settings/files/zzz-default-settings
+
+#3. Replace with Argon
+rm openwrt/package/lean/luci-theme-argon -rf
+
+#4. Install JerryKuKu’s new Argon
+git clone -b 18.06 https://github.com/jerrykuku/luci-theme-argon.git openwrt/package/lean
+
+#5. Change the argon ico
+

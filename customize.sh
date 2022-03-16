7fc79924801a436bf000d3cd6035d43dcdce296f
frehedhuang <87220226+frehedhuang@users.noreply.github.com>
2022-03-16 20:29:15 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -6,13 +6,13 @@
 # Youtube Channel: https://goo.gl/fvkdwm 
 #=================================================
 #1. Modify default IP
-sed -i 's/192.168.1.1/192.168.100.1/g' openwrt/package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' openwrt/package/base-files/files/bin/config_generate
 
 #2. Clear the login password
 sed -i 's/$1$V4UetPzk$CYXluq4wUazHjmCDBCqXF.//g' openwrt/package/lean/default-settings/files/zzz-default-settings
 
 #3. Replace with JerryKuKu’s Argon
-rm openwrt/package/lean/luci-theme-argon -rf
+#rm openwrt/package/lean/luci-theme-argon -rf
 
 #4.change hostname
-sed -i '/uci commit system/i\uci set system.@system[0].hostname='Mi-WiFi'' openwrt/package/lean/default-settings/files/zzz-default-settings
+#sed -i '/uci commit system/i\uci set system.@system[0].hostname='Mi-WiFi'' openwrt/package/lean/default-settings/files/zzz-default-settings
